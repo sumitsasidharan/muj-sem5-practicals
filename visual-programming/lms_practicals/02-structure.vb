2032537bb9eeae1b299c863e8b977b75d5271edf
sumitsasidharan <s.sumit196@gmail.com>
2024-12-22 22:00:09 +0530

    web design ia, vb.net lms

---
+++ visual-programming/lms_practicals/02-structure.vb
@@ -0,0 +1,18 @@
+' Exercise 2. Write a program to create a Structure in VB.Net.
+
+
+Module Module1
+  Structure Struct
+    Public x As Integer
+    Public y As Integer
+  End Structure
+
+  Sub Main()
+    Dim st As New Struct
+    st.x = 10
+    st.y = 20
+    Dim sum As Integer = st.x + st.y
+    Console.WriteLine("The result is {0}", sum)
+    Console.ReadKey()
+  End Sub
+End Module
